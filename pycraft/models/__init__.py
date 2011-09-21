ac145d789e8ce3e606ea6f791c33ca48232ab4ed
Mark McGuire <mark.b.mcg@gmail.com>
2011-09-20 23:48:53 -0500

    locking and proc control

--- pycraft/models/__init__.py
+++ pycraft/models/__init__.py
@@ -8,8 +8,11 @@ class PyCraft(PersistentMapping):
   base_path = './pycraft_store'
 
 class Servers(PersistentMapping):
+  __acl__ = None
   __name__ = 'servers'
-  base_path = __parent__.base_path + '/servers'
+  #paths
+  servers_path = __parent__.base_path + '/servers'
+  backups_path = None
 
   def __init__(self):
     #list of running servers
@@ -17,21 +20,27 @@ class Servers(PersistentMapping):
 
   def start_all(self):
     for server in self.values():
+      server.lock().acquire()
       server.start()
+      server.lock().release()
 
   def start_list(self, svr_list):
     for server in svr_list:
+      server.lock().acquire()
       server.start()
+      server.lock().release()
 
   #stop all running servers gracefully
   def stop_all(self):
     for server in self.running:
+      server.lock().acquire()
       server.stop()
+      server.lock().release()
 
   def restart_all_running(self):
     start = self.running
     self.stop_all()
-    self.start(running)
+    self.start_list(running)
 
   #force stop all servers
   def force_stop_all(self):
@@ -40,11 +49,15 @@ class Servers(PersistentMapping):
 
   def save_all(self):
     for server in self.running:
+      server.lock().acquire()
       server.save()
+      server.lock().release()
 
   def backup_all(self):
     for server in self.values():
+      server.lock().acquire()
       server.backup()
+      server.lock().release()
 
   #map all servers with a mapping command
   #check that a mapper is defined
@@ -52,7 +65,9 @@ class Servers(PersistentMapping):
     #if not self.map_command:
       #raise MapError(MapError.cmd_undef)
     for server in self.values():
+      server.lock().acquire()
       server.make_map()
+      server.lock().release()
 
 #set up database on first run and
 #return our application root
--- pycraft/models/server.py
+++ pycraft/models/server.py
@@ -1,36 +1,108 @@
-from persistent import Persistent
+from persistent import Persistent, PersistentList
+from threading import RLock
+from subprocess import Popen, PIPE
 
 class Server(Persistent):
   def __init__(self, name, path):
     self.__name__ = name
+    self.__acl__ = None
     self.last_save = None
     self.last_bkup = None
-    self.lock = False
+    self.path = None
+    self.cmd_args = None
+    self.userlist = PersistentList()
+    #connection to the minecraft shell
+    #self._v_shell = None
+    #lock to allow our multithreaded app
+    #to block on multiple requests
+    #self.lock = RLock()
     self.make_server(path)
 
-  def make_server(self, path):
-    self.get_lock()
-    self.path = path
-    self.release_lock()
+#VOLITILE ATTRIBUTES
+  def lock(self):
+    if hasattr(self, '_v_lock'):
+      return self._v_lock
+    lock = RLock(False)
+    self._v_lock=lock
+    return lock
 
-  def get_lock(self):
-    pass
+  def proc(self):
+    if hasattr(self, '_v_proc'):
+      return self._v_proc
+
+  def connected_userlist(self):
+    if hasattr(self, '_v_conn_userlist'):
+      return self._v_conn_userlist
+
+#END VOLITILE ATTRIBUTES
+
+#LOCK METHODS
+  def acquire_lock(self, t):
+    if not t:
+      return self.lock().acquire()
+    #convert t to a time
+    now = time()
+    end = now + t
+    while(!self.lock().acquire() && now<end):
+      sleep(.3)
+      now = time()
+    return self.lock().acquire()
 
   def release_lock(self):
-    pass
+    self.lock().release()
+#END LOCK METHODS
+
+  def make_server(self, path):
+    if self.acquire_lock():
+      try:
+        self.path = path
+      finally:
+        self.release_lock()
 
   def start(self):
-    get_lock()
-    release_lock()
+    if self.acquire_lock():
+      try:
+        self._v_proc = Popen(self.cmd_args, stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=self.path)
+        #wait for server to start
+      finally:
+        self.release_lock()
 
   def stop(self):
-    get_lock()
-    release_lock()
+    if self.acquire_lock():
+      try:
+        if (proc = self.proc()):
+          proc.communicate('say SERVER SHUTTING DOWN')
+          self.save()
+          #check for save complete
+          proc.communicate('stop')
+      self.release_lock()
 
   def force_stop(self):
-    get_lock()
-    release_lock()
+    #force release lock/stop server
+    if (proc = self.proc()):
+      proc.kill
+
+  def save(self):
+    if self.acquire_lock():
+      try:
+        if (proc = self.proc()):
+          proc.communicate('save-on')
+          proc.communicate('save-all')
+          #wait for save to complete
+          proc.communicate('save-off')
+      finally:
+        self.release_lock()
 
   def make_map(self):
-    get_lock()
-    release_lock()
+    if self.acquire_lock():
+      try:
+        pass
+      finally:
+        self.release_lock()
+    
+  def backup(self):
+    if self.lock().acquire():
+      try:
+        pass
+      finally:
+        self.lock().release()
